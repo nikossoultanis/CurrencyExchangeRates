2f3a08bbb50522284c43866ad3a49aa7a06f3f04
nikossoultanis <nikossoultanis@yahoo.com>
2025-06-27 18:04:49 +0300

    add the model for Currency Rate inside the Domain Layer

---
+++ CurrencyExchangeRates.Domain/Models/CurrencyRate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyExchangeRates.Domain.Models
+{
+    public class CurrencyRate
+    {
+        public int Id { get; set; }
+        public string CurrencyCode { get; set; } = null!;
+        public decimal Rate { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
