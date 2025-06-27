58741e50b38a62227ab57f9c25dc1a1d0099427c
nikossoultanis <nikossoultanis@yahoo.com>
2025-06-27 18:55:25 +0300

    CurrencyRate Entity inside the Domain Layer And Create DbContextFactory, and AppDbContext. Factory is needed in order to migrate

---
+++ CurrencyExchangeRates.Domain/Entities/CurrencyRate.cs
@@ -0,0 +1,11 @@
+﻿
+namespace CurrencyExchangeRates.Domain.Entities
+{
+    public class CurrencyRate
+    {
+        public int Id { get; set; }
+        public string CurrencyCode { get; set; } = null!;
+        public decimal Rate { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
