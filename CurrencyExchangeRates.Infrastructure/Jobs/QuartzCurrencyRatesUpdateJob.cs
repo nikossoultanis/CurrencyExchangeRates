2dfca6a6695390a2f99bbe5e81a2d218530e9b72
nikossoultanis <nikossoultanis@yahoo.com>
2025-06-27 18:57:19 +0300

    create a quartz wrapper for the job we created and inject it into Depedency Injection file

---
+++ CurrencyExchangeRates.Infrastructure/Jobs/QuartzCurrencyRatesUpdateJob.cs
@@ -0,0 +1,21 @@
+﻿using CurrencyExchangeRates.Application.Common.Jobs.Interfaces;
+using Quartz;
+
+namespace CurrencyExchangeRates.Infrastructure.Jobs
+{
+
+    public class QuartzCurrencyRatesUpdateJob : IJob
+    {
+        private readonly ICurrencyRatesUpdateJob _job;
+        public QuartzCurrencyRatesUpdateJob(ICurrencyRatesUpdateJob job)
+        {
+            _job = job;
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await _job.ExecuteAsync(context.CancellationToken);
+        }
+    }
+
+}
