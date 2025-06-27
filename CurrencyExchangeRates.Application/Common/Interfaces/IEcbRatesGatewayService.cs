2ddc826d47855fae3c3e197f47c6fc1533d63232
nikossoultanis <nikossoultanis@yahoo.com>
2025-06-27 20:21:46 +0300

    rename interface and service for EcbRatesGateway

---
+++ CurrencyExchangeRates.Application/Common/Interfaces/IEcbRatesGatewayService.cs
@@ -0,0 +1,9 @@
+﻿using CurrencyExchangeRates.Domain.Entities;
+
+namespace CurrencyExchangeRates.EcbGateway.Services.Interfaces
+{
+    public interface IEcbRatesGatewayService
+    {
+        Task<List<CurrencyRate>> GetDailyRatesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ CurrencyExchangeRates.EcbGateway/Services/Implementations/EcbRatesGatewayService.cs
@@ -0,0 +1,49 @@
+﻿using CurrencyExchangeRates.Domain.Entities;
+using CurrencyExchangeRates.EcbGateway.Services.Interfaces;
+using System.Globalization;
+using System.Xml.Linq;
+
+namespace CurrencyExchangeRates.EcbGateway.Services.Implementations
+{
+    public class EcbRatesGatewayService : IEcbRatesGatewayService
+    {
+        private readonly HttpClient _httpClient;
+        private const string EcbUrl = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml";
+
+        public EcbRatesGatewayService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<CurrencyRate>> GetDailyRatesAsync(CancellationToken cancellationToken = default)
+        {
+            var xml = await _httpClient.GetStringAsync(EcbUrl);
+
+            var xdoc = XDocument.Parse(xml);
+            var ns = xdoc.Root.GetDefaultNamespace();
+
+            // Find the Cube with the time attribute
+            var timeCube = xdoc.Descendants(ns + "Cube")
+                               .FirstOrDefault(x => x.Attribute("time") != null);
+
+            if (timeCube == null)
+                throw new Exception("ECB XML does not contain expected time node.");
+
+            var date = DateTime.Parse(timeCube.Attribute("time").Value);
+
+            var rates = timeCube.Elements()
+                .Select(x => new CurrencyRate
+                {
+                    CurrencyCode = x.Attribute("currency")?.Value,
+                    Rate = decimal.Parse(
+                        x.Attribute("rate")?.Value ?? "0",
+                        NumberStyles.Any,
+                        CultureInfo.InvariantCulture),
+                    Date = date
+                })
+                .ToList();
+
+            return rates;
+        }
+    }
+}
